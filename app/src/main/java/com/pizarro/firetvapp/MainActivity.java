49eb7b55d65b62d837e02a6cb740a618c403afb8
agustino8080 <agispin135@gmail.com>
2026-07-16 16:56:57 -0500

    Initial commit: FireTV WebView App

---
+++ app/src/main/java/com/pizarro/firetvapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.pizarro.firetvapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.view.KeyEvent;
+
+public class MainActivity extends Activity {
+
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        myWebView = new WebView(this);
+        setContentView(myWebView);
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        myWebView.setWebViewClient(new WebViewClient());
+
+        // Cambia esta URL por la de tu web final
+        myWebView.loadUrl("https://www.google.com"); 
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
